346e56dee608adeae3cf1f72bde1ae197f8e7ff5
grantorchard <go@hashicorp.com>
2019-12-03 18:09:44 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,12 @@ data terraform_remote_state "certificates" {
   }
 }
 
+null_resource "echo" {
+  provisioner "local-exec" {
+    inline = [
+      "echo ${data.terraform_remote_state.certificates.outputs.vault_certificate}"
+    ]
+
 output "vault_certificate" {
   value = data.terraform_remote_state.certificates.outputs.vault_certificate
   }
